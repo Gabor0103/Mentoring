b6cd7e4accf9aee5c786d975a0cf1307bfe883e5
v-gamolnar <v-gamolnar@expedia.com>
2016-06-10 08:42:02 +0200

    Operator overloading?

---
+++ ProjectGenerics2/src/com/gabor/main/FinalGeneric.java
@@ -0,0 +1,19 @@
+package com.gabor.main;
+
+import java.util.Comparator;
+
+public final class FinalGeneric<T extends Comparable<T>> implements Comparator<T> 
+{
+    public static <T> T max(T x, T y) {
+    	
+    	//A "T" típus nem definiál semmilyen operátort, nekünk kellene az összehasonlítást megcsinálni(?)
+        return x > y ? x : y;
+    }
+
+	@Override
+	public int compare(T o1, T o2)
+	{
+		return o1.compareTo(o2);
+	}
+
+}
